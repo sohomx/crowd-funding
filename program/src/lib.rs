4b63c1fe740d6f342d22388e55b88b21779323c8
sohom <sohom377@gmail.com>
2022-01-27 14:04:04 +0530

    Withdraw function implementation

--- program/src/lib.rs
+++ program/src/lib.rs
@@ -1,3 +1,5 @@
+
+use borsh::{BorshDeserialize, BorshSerialize};
 use solana_program::{
     account_info::{next_account_info, AccountInfo},
     entrypoint,
@@ -8,7 +10,6 @@ use solana_program::{
     rent::Rent,
     sysvar::Sysvar,
 };
-
 fn process_instruction(
     program_id: &Pubkey,
     accounts: &[AccountInfo],
@@ -18,27 +19,107 @@ fn process_instruction(
         return Err(ProgramError::InvalidInstructionData);
     }
 
-    // 0 = create_campaign, 1 = withdraw, 2 = donate
     if instruction_data[0] == 0 {
         return create_campaign(
             program_id,
             accounts,
             &instruction_data[1..instruction_data.len()],
         );
-    } else if instruction_data[0] == 2 {
-        return withdraw (
+    } else if instruction_data[0] == 1 {
+        return withdraw(
             program_id,
             accounts,
             &instruction_data[1..instruction_data.len()],
         );
     } else if instruction_data[0] == 2 {
-        return donate (
+        return donate(
             program_id,
             accounts,
-            &instruction_data[1..instruction_data.len()]
-        )
+            &instruction_data[1..instruction_data.len()],
+        );
     }
+    msg!("Didn't found the entrypoint required");
+    Err(ProgramError::InvalidInstructionData)
+}
+entrypoint!(process_instruction);
+
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct CampaignDetails {
+    pub admin: Pubkey,
+    pub name: String,
+    pub description: String,
+    pub image_link: String,
+    pub amount_donated: u64,
+}
+
+fn create_campaign(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let writing_account = next_account_info(accounts_iter)?;
+    let creator_account = next_account_info(accounts_iter)?;
+    if !creator_account.is_signer {
+        msg!("creator_account should be signer");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    if writing_account.owner != program_id {
+        msg!("writing_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let mut input_data = CampaignDetails::try_from_slice(&instruction_data)
+        .expect("Instruction data serialization didn't worked");
+
+    if input_data.admin != *creator_account.key {
+        msg!("Invaild instruction data");
+        return Err(ProgramError::InvalidInstructionData);
+    }
+    let rent_exemption = Rent::get()?.minimum_balance(writing_account.data_len());
+    if **writing_account.lamports.borrow() < rent_exemption {
+        msg!("The balance of writing_account should be more then rent_exemption");
+        return Err(ProgramError::InsufficientFunds);
+    }
+    input_data.amount_donated = 0;
+    input_data.serialize(&mut &mut writing_account.try_borrow_mut_data()?[..])?;
     Ok(())
 }
 
-entrypoint!(process_instruction);
+#[derive(BorshSerialize, BorshDeserialize, Debug)]
+struct WithdrawRequest {
+    pub amount: u64,
+}
+
+fn withdraw(
+    program_id: &Pubkey,
+    accounts: &[AccountInfo],
+    instruction_data: &[u8],
+) -> ProgramResult {
+    let accounts_iter = &mut accounts.iter();
+    let writing_account = next_account_info(accounts_iter)?;
+    let admin_account = next_account_info(accounts_iter)?;
+
+    if writing_account.owner != program_id{
+        msg!("writing_account isn't owned by program");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+    // Admin account should be the signer in this trasaction.
+    if !admin_account.is_signer {
+        msg!("admin should be signer");
+        return Err(ProgramError::IncorrectProgramId);
+    }
+
+    let campaign_data = CampaignDetails::try_from_slice(*writing_account.data.borrow())
+        .expect("Error deserializing data");
+    
+    // we check if the admin_account's public key is equal to the public key we have stored in our campaign_data
+    if campaign_data.admin != *admin_account.key {
+        msg!("admin_account's public key is not equal to the public key we have stored in our campaign_data");
+        return Err(ProgramError::InvalidInstructionData);
+    }  
+
+    // we will get the amount of lamports admin wants to withdraw
+    let input_data = WithdrawRequest::try_from_slice(&instruction_data).expect("Instruction data serialization didn't")
+}
